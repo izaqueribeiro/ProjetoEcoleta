5b882814bf5405bfe704c3d44e3b04ca8145a50d
izaqueribeiro <113313801+izaqueribeiro@users.noreply.github.com>
2022-12-08 09:37:56 -0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+
+const buttonSearch = document.querySelector("#search-results main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+
+close.addEventListener("click", () => {
+    
+    modal.classList.add('hide')
+    
+})
+
+
